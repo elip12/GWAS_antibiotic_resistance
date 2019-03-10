d9f4e8860a9ee28daffed139d3bda6bbbffa2c72
Eli 2 <eli2@KOAL.local>
2019-03-09 22:38:02 -0800

    update stuff

--- gwas2.py
+++ gwas2.py
@@ -3,6 +3,7 @@ import multiprocessing as mp, os, pickle, argparse
 DEBUG = False
 K = 30
 
+### AUXILLIARY FUNCTIONS ###
 def printd(*args, **kwargs):
     if DEBUG == True:
         print(*args, **kwargs)
@@ -13,6 +14,7 @@ def load_raw():
         raw = pickle.load(f)
     return raw
 
+# complements a single base
 def complement(base):
     if base == 'A':
         return 'T'
@@ -23,12 +25,32 @@ def complement(base):
     else:
         return 'G'
 
+# breaks input file into chunks to minimize reads
+def chunkify(fname,size=512):
+    fileEnd = os.path.getsize(fname)
+    with open(fname,'rb') as f:
+        chunkEnd = f.tell()
+        while True:
+            chunkStart = chunkEnd
+            f.seek(size,1)
+            f.readline()
+            chunkEnd = f.tell()
+            yield chunkStart, chunkEnd - chunkStart
+            if chunkEnd > fileEnd:
+                break
+
+### K-MER DATABASE FUNCTIONS ###
+
+# writes a kmer doct to a file
+# all in 1 single write to hopefully minimize data races
 def write_kmers(kmers):
     printd('Writing kmers to file...')
     with open('output.txt', 'a+') as f:
         s = '\n'.join([f'{k}{v}' for k,v in kmers.items()]) + '\n'
         f.write(s)
 
+# assigns locations to kmers in kmer dict by iterating through all
+# genomes
 def process(kmers):
     printd('Extracting kmer locations...')
     for count, (raw_id, raw_dict) in enumerate(raw.items()):
@@ -60,19 +82,28 @@ def process_wrapper(chunkStart, chunkSize):
     process(kmers)
     # print(len(kmers))
 
-# breaks input file into chunks to minimize reads
-def chunkify(fname,size=1024):
-    fileEnd = os.path.getsize(fname)
-    with open(fname,'rb') as f:
-        chunkEnd = f.tell()
-        while True:
-            chunkStart = chunkEnd
-            f.seek(size,1)
-            f.readline()
-            chunkEnd = f.tell()
-            yield chunkStart, chunkEnd - chunkStart
-            if chunkEnd > fileEnd:
-                break
+
+### Classify by population structure ###
+
+# 1. DSK for kmer input; we are going to use kmers of abundance > 2
+def read_full_input(fname):
+    kmers = {}
+    with open(fname, 'r') as f:
+        for line in f:
+            kmer = line.split(' ')[0]
+            comp = ''.join(map(complement, list(kmer)))
+            kmers[kmer] = ''
+            kmers[comp] = ''
+    return kmers
+
+# 2. Sample 1%
+
+# 3. Distance Matrix
+# call distance matrix on this 
+# 4. Cluster from matrix
+
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Runs GWAS on microbial genomes')
@@ -87,19 +118,19 @@ if __name__ == '__main__':
     jobs = []
 
     # create jobs
-    n = 0
+    # n = 0
     for chunkStart,chunkSize in chunkify('input.txt'):
-        n += 1
-        if n > 4:
-            break
-        printd(f'Starting chunk {n}...')
+        # n += 1
+        # if n > 4:
+            # break
+        # printd(f'Starting chunk {n}...')
         jobs.append(pool.apply_async(process_wrapper,(chunkStart,chunkSize)))
 
     # wait for all jobs to finish
-    n = 0
+    # n = 0
     for job in jobs:
         job.get()
-        n += 1
-        printd(f'Finished chunk {n}...')
+        # n += 1
+        # printd(f'Finished chunk {n}...')
 
     pool.close()
